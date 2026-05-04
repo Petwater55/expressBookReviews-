5681a885865e9364e08e0c78eb47d6f79664ecf2
Petwater55 <Justbringit4728@gmail.com>
2026-05-04 15:50:41 -0400

    message

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,8 +11,8 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
+public_users.get('/books/:books',function (req, res) {
+  res.send(JSON.stringify(books, null, 10 ));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
