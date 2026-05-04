8516102441d0e8768493c3541112084af85a8b1f
Petwater55 <Justbringit4728@gmail.com>
2026-05-04 14:40:18 -0400

    Internal message

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+const { authenticated } = require('./router/auth_users.js');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -11,7 +12,19 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if(authenticatedUser(username, password)) {
+        // Generated JWT access token
+        let accesstoken = jwt.sign({
+            data: password
+    }, 'access', {expiresIn: 60 * 60 }); 
+    // This stores the access token and username in session
+    req.session.autherization ={
+        accessToken, username
+    }
+    return res.status(200).send('User successfully logged in! ');
+} else {
+    return res.status(208).json({message: "Invalid login. Check username and password"});
+}
 });
  
 const PORT =5000;
