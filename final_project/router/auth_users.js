9b10012f5225882f1bf50a607a306b1e9cd61d4e
Petwater55 <Justbringit4728@gmail.com>
2026-05-05 15:37:21 -0400

    message

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,14 +9,27 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username,password)=>{ 
+    return users.some(user => user.username === username && user.password === password);  
+};
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+//only registered users can login task 7
+regd_users.post("/customer/login", (req,res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required"});
+  }
+  if (authenticatedUser(username, password)) {
+    const accesstoken = jwt.sign({ username: username}, 'access', { expiresIn: '1h' });
+    req.session.authorization = {
+        accesstoken, 
+        username
+    };
+
+  return res.status(200).json({message: "User successfully logged in", accesstoken });
+} else {
+    return res.status(401).json({message: "Invalid username or password"});
+}
 });
 
 // Add a book review
